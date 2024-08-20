d6ee67d491c5aa7a884bfcfddd9500e212f238fc
Matheus Alcance <limarios18@outlook.com>
2024-08-20 00:16:04 -0300

    Ajustando erro de relacionamento de banco de dados

--- models/models.py
+++ models/models.py
@@ -1,7 +1,7 @@
 from application import db
 from datetime import datetime
-
-
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 
 class Usuarios(db.Model):
     nickname = db.Column(db.String(8), primary_key=True)
@@ -32,39 +32,41 @@ class User(db.Model):
 
 class Customer(db.Model):
     __tablename__ = 'customer'
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    razao_social = db.Column(db.String(200), nullable=False)
-    nome_fantasia = db.Column(db.String(200), nullable=False)
-    cpfecnpj = db.Column(db.String(30))
-    inscricao_estadual = db.Column(db.String(50))
-    inscricao_municipal = db.Column(db.String(50))
-    telefone = db.Column(db.String(50))
-    celular = db.Column(db.String(50))
-    email = db.Column(db.String(100))
-    tipo_conta = db.Column(db.String(20))
-    endereco = db.Column(db.String(100))
-    bairro = db.Column(db.String(100))
-    cidade = db.Column(db.String(100))
-    estado = db.Column(db.String(5))
-    numero = db.Column(db.String(10))
-    cep = db.Column(db.String(10))
-    complemento = db.Column(db.String(100))
-    activities = db.relationship('Activity', backref='cliente', lazy=True)
+    id = db.Column(Integer, primary_key=True, unique=True)
+    razao_social = db.Column(String(200), nullable=False)
+    nome_fantasia = db.Column(String(200), nullable=False)
+    cpfecnpj = db.Column(String(30))
+    inscricao_estadual = db.Column(String(50))
+    inscricao_municipal = db.Column(String(50))
+    telefone = db.Column(String(50))
+    celular = db.Column(String(50))
+    email = db.Column(String(100))
+    tipo_conta = db.Column(String(20))
+    endereco = db.Column(String(100))
+    bairro = db.Column(String(100))
+    cidade = db.Column(String(100))
+    estado = db.Column(String(5))
+    numero = db.Column(String(10))
+    cep = db.Column(String(10))
+    complemento = db.Column(String(100))
+
+    # Relacionamento correto com Activity
+    activities = db.relationship('Activity', backref='customer')
 
     def __repr__(self):
-        return '<name %r' % self.__name__
+        return '<Customer %r>' % self.nome_fantasia
 
 class Activity(db.Model):
     __tablename__ = 'activity'
-    id = db.Column(db.Integer, primary_key=True)
-    cliente_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    data_inicio = db.Column(db.DateTime, default=datetime.now)
-    data_fim = db.Column(db.DateTime)
-    tipo = db.Column(db.String(100))
-    titulo = db.Column(db.String(100))
+    id = db.Column(Integer, primary_key=True)
+    cliente_id = db.Column(Integer, ForeignKey('customer.id'), nullable=False)
+    usuario_id = db.Column(Integer, ForeignKey('user.id'), nullable=False)
+    data_inicio = db.Column(DateTime, default=datetime.now)
+    data_fim = db.Column(DateTime)
+    tipo = db.Column(String(100))
+    titulo = db.Column(String(100))
     descricao = db.Column(db.Text)
-    status = db.Column(db.String(20))
+    status = db.Column(String(20))
 
     def __repr__(self):
         return '<name %r' % self.__name__
