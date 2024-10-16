4e7d53cb04a3ff2cced60d1ffb74b0aa57b6d457
Matheus Alcance <limarios18@outlook.com>
2024-10-16 19:43:15 -0300

    Ajustando metodo para listar as propostas na pagina de listagem geral

--- view/views_proposal.py
+++ view/views_proposal.py
@@ -4,6 +4,48 @@ from models.models import Activity, User, Customer, Prospect, Proposal
 from helpers.forms_helpers import FormActivity, is_admin, FormProposal
 from datetime import datetime
 
+
+@app.route('/proposal')
+def proposal():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proxima=url_for('proposal')))
+
+    adm = is_admin()
+    form = FormProposal()
+
+    # Pegando o cliente e prospect dos parâmetros de busca (opcional)
+    cliente_id = request.args.get('cliente_id', type=int)
+    prospect_id = request.args.get('prospect_id', type=int)
+    search = request.args.get('search', '')
+
+    page = request.args.get('page', 1, type=int)
+
+    # Construção da query, especificando o onclause no join para evitar ambiguidade
+    query = db.session.query(Proposal).join(Customer, Proposal.customer_id == Customer.id)
+
+    # Join explícito com a tabela User usando a chave estrangeira 'usuario_id'
+    # query = query.join(User, Proposal.usuario_id == User.id)
+    query = query.join(User, Proposal.responsavel_id == User.id, isouter=True)  # Join com 'responsavel_id' (opcional)
+
+    # Aplicar filtro de busca se disponível
+    if cliente_id:
+        query = query.filter(Proposal.customer_id == cliente_id)
+
+    if search:
+        query = query.filter(Proposal.nome.ilike(f'%{search}%'))  # Busca por nome
+
+    propostas = query.order_by(Proposal.data_criacao.desc()).paginate(page=page, per_page=10)
+
+    return render_template('proposal/proposals.html',
+                           titulo='Propostas Comerciais',
+                           propostas=propostas,
+                           clientes=Customer.query.all(),
+                           prospects=Prospect.query.all(),
+                           is_admin=adm,
+                           form=form
+                           )
+
+
 @app.route('/proposta/nova', methods=['GET', 'POST'])
 def created_proposal():
     form = FormProposal()
