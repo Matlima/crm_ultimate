8cd753ee00498010dfb238327b7fbaf63789cd72
Matheus Alcance <limarios18@outlook.com>
2024-10-25 18:55:26 -0300

    Criando primeira rota para listar a carteira do usuário logado

---
+++ view/views_portfolio_user.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, redirect, session, flash, url_for
+from application import app, db
+from models.models import Customer, CustomerPortfolio, User, PortfolioItem, Prospect
+from helpers.forms_helpers import FormCustomerPortfolio, is_admin, FormPortfolioItem
+from datetime import datetime
+
+
+# Methods Routes:
+@app.route('/my_portfolio')
+def my_portfolio_customers_prospect():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proxima=url_for('dashboard')))
+
+    adm = is_admin()
+    usuario_id = session["usuario_id"]
+    user = User.query.get(usuario_id)
+
+    # Pega todos os portfólios onde o usuário logado é o responsável
+    portfolio_user = CustomerPortfolio.query.filter_by(responsavel_id=user.id).all()
+
+    # Extrai todos os IDs de portfólio para filtrar os itens
+    portfolio_ids = [portfolio.id for portfolio in portfolio_user]
+
+    # Filtra os itens de portfólio onde o portfolio_id está na lista de IDs dos portfólios do usuário
+    page = request.args.get('page', 1, type=int)
+    itens_carteiras = PortfolioItem.query.filter(PortfolioItem.portfolio_id.in_(portfolio_ids)).paginate(page=page,
+                                                                                                         per_page=10)
+
+    return render_template('my_user/my_customers_and_prospects.html',
+                           titulo='Minha carteira',
+                           itens_carteiras=itens_carteiras,
+                           is_admin=adm
+                           )
+
