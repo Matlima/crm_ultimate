9f55b8bad158832d4eab918b70b3d45be89d42b0
Matheus Alcance <limarios18@outlook.com>
2024-11-18 16:42:13 -0300

    Criando função para adicionar atividade no cliente ou prospect dentro do portfolio do usuario

--- view/views_portfolio_user.py
+++ view/views_portfolio_user.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, session, flash, url_for
 from application import app, db
-from models.models import Customer, CustomerPortfolio, User, PortfolioItem, Prospect
+from models.models import Customer, CustomerPortfolio, User, PortfolioItem, Prospect, Activity
 from helpers.forms_helpers import FormCustomerPortfolio, is_admin, FormPortfolioItem, FormActivity
 from datetime import datetime
 
@@ -41,8 +41,7 @@ def my_portfolio_new_activity(tipo, id):
     usuario_id = session["usuario_id"]
     adm = is_admin()
     # grupo = type_user()
-    tipo = request.args.get('tipo')
-    id = request.args.get('id')
+
     return render_template('activities/add_activity_my.html',
                            titulo='Nova atividade',
                            form=form,
@@ -53,3 +52,64 @@ def my_portfolio_new_activity(tipo, id):
                            id=id,
                            tipo=tipo
                            )
+
+@app.route('/my_portfolio/activities/add', methods=['POST'])
+def my_portfolio_add_activity():
+    if 'usuario_logado' not in session or session['usuario_logado'] is None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+
+    usuario_id = session["usuario_id"]
+
+    form = FormActivity(request.form)
+    # Obtenha os valores do formulário
+    id = request.form.get('id')
+    tipo = request.form.get('tipo')
+
+
+    # Verifica se id está presente e é válido
+    if id and id.isdigit():
+        id = int(id)  # Converte para inteiro se válido
+    else:
+        flash('ID inválido ou não fornecido.', 'error')
+        return redirect(url_for('my_portfolio_customers_prospect'))
+
+
+    # Obtém os demais dados do formulário
+    status = form.status.data
+    titulo = form.titulo.data
+    descricao = form.descricao.data
+    tipo2 = form.tipo.data
+
+    # Verifica o tipo e cria a atividade correspondente
+    if tipo == 'cliente':
+        new_activity = Activity(
+            data_inicio=datetime.now(),
+            data_fim=datetime.now(),
+            titulo=titulo,
+            descricao=descricao,
+            tipo=tipo2,
+            status=status,
+            cliente_id=id,
+            usuario_id=usuario_id
+        )
+    elif tipo == 'prospect':
+        new_activity = Activity(
+            data_inicio=datetime.now(),
+            data_fim=datetime.now(),
+            titulo=titulo,
+            descricao=descricao,
+            tipo=tipo2,
+            status=status,
+            prospect_id=id,
+            usuario_id=usuario_id
+        )
+    else:
+        flash('Tipo inválido!', 'error')
+        return redirect(url_for('my_portfolio_customers_prospect'))
+
+    # Salva a nova atividade no banco de dados
+    db.session.add(new_activity)
+    db.session.commit()
+
+    flash('Atividade adicionada com sucesso!')
+    return redirect(url_for('my_portfolio_customers_prospect'))
