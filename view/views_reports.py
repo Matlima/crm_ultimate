33d086873a243090e979709e0748d54cd2061ee0
Matheus Alcance <limarios18@outlook.com>
2024-11-23 20:50:23 -0300

    Criando primeira rota para ir para relatorios

---
+++ view/views_reports.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
+from application import app, db
+from models.models import User, Activity
+from helpers.forms_helpers import FormUser
+from flask_bcrypt import generate_password_hash
+from sqlalchemy.exc import IntegrityError
+
+@app.route('/reports')
+def report():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('dashboard')))
+
+    return render_template('reports/reports.html',
+                           titulo='Relatórios'
+                           )
